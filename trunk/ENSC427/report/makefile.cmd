fa284ffbb01bef95fc45e02e30c4fe709e1196d1
dan.mangoman <dan.mangoman@a5d5ea38-08f6-11de-98bb-07cc9828235c>
2010-03-14 04:57:24 +0000

    Added basic report framework

---
+++ trunk/ENSC427/report/makefile.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+set FILENAME=finalReport
+
+set BIBNAME=finalReport
+
+
+
+pdflatex --interaction nonstopmode %FILENAME%
+
+bibtex %FILENAME%
+
+pdflatex --interaction nonstopmode %FILENAME% 
+
+pdflatex --interaction nonstopmode %FILENAME%
+
+
+
+del *.dvi *.ps *.aux *.log *.bbl *.blg *.out *.lot *.lof *.toc
