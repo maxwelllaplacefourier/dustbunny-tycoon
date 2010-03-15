80e9bc17f3dd231f6d4ee4912c3cf37c504a662e
dan.mangoman <dan.mangoman@a5d5ea38-08f6-11de-98bb-07cc9828235c>
2010-03-15 03:37:50 +0000

    Makefile update

--- ENSC427/report/makefile.cmd
+++ ENSC427/report/makefile.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+set path=%path%;"C:\Program Files\MiKTeX 2.8\miktex\bin"
+
 set FILENAME=finalReport
 
 set BIBNAME=finalReport
@@ -14,9 +16,11 @@ pdflatex --interaction nonstopmode %FILENAME%
 
 pdflatex --interaction nonstopmode %FILENAME%
 
+echo.
 echo Done compile, going to clean
 pause
 
+echo.
 call clean.cmd
 echo Done clean, going to exit
 pause
