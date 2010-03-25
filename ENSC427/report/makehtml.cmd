1cc9c19034667129b580070e0e8892227729be39
dan.mangoman <dan.mangoman@a5d5ea38-08f6-11de-98bb-07cc9828235c>
2010-03-25 14:48:56 +0000

    A few small style updates.
    Also added a new makehtml command - it would be good to generate the webpage dynamically.

---
+++ ENSC427/report/makehtml.cmd
@@ -0,0 +1,86 @@
+@echo off
+setlocal
+
+set FILENAME=finalReport
+
+set BIBNAME=finalReport
+
+
+:: For timbo - paths not set up right
+if exist "C:\Program Files\MiKTeX 2.8\miktex\bin" set path=%path%;"C:\Program Files\MiKTeX 2.8\miktex\bin"&&echo Updated PATH
+
+::###########################################
+:: Delete current HTML
+::###########################################
+
+if exist %FILENAME%.html (
+  echo Going to delete current HTML
+  pause
+
+  del %FILENAME%.html
+
+  if exist %FILENAME%.html (
+    echo.
+    echo Could not delete
+    goto :END
+  )
+)
+
+::###########################################
+:: Clean 
+::###########################################
+
+echo.
+echo Going to clean
+
+echo.
+call clean.cmd
+
+::###########################################
+:: Compile
+::###########################################
+
+latex %FILENAME%
+
+
+::pause
+
+bibtex %FILENAME%
+
+
+htlatex %FILENAME%
+
+
+if not exist %FILENAME%.html (
+  echo Not generate, please look up to see if there were any errors
+  goto :END
+)
+
+::###########################################
+:: Clean
+::###########################################
+
+::DISABLED
+
+::echo.
+::echo Done compile, going to clean
+
+::echo.
+::call clean.cmd
+
+
+::###########################################
+:: Show it
+::###########################################
+
+echo Opening 
+%FILENAME%.html
+
+::###########################################
+:: END
+::###########################################
+:END
+
+echo.
+echo Done, going to exit
+pause
