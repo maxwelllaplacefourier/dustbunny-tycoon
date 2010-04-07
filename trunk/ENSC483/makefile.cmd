4c161983a677668ac67fe8e4286536b8577f47fb
dan.mangoman <dan.mangoman@a5d5ea38-08f6-11de-98bb-07cc9828235c>
2010-04-07 19:00:45 +0000

    483 Project template

---
+++ trunk/ENSC483/makefile.cmd
@@ -0,0 +1,86 @@
+@echo off
+setlocal
+
+set FILENAME=project
+set BIBNAME=References
+
+
+:: For timbo - paths not set up right
+if exist "C:\Program Files\MiKTeX 2.8\miktex\bin" set path=%path%;"C:\Program Files\MiKTeX 2.8\miktex\bin"&&echo Updated PATH
+
+::###########################################
+:: Delete current PDF
+::###########################################
+
+if exist %FILENAME%.pdf (
+  echo Going to delete current PDF
+  pause
+
+  del %FILENAME%.pdf
+
+  if exist %FILENAME%.pdf (
+    echo.
+    echo Could not delete pdf, please close it if it is open
+    goto :END
+  )
+)
+
+::###########################################
+:: Clean 
+::###########################################
+
+echo.
+echo Going to clean
+
+echo.
+call clean.cmd
+
+::###########################################
+:: Compile
+::###########################################
+
+::pdflatex --interaction nonstopmode %FILENAME%
+pdflatex %FILENAME%
+
+
+::pause
+
+bibtex %FILENAME%
+
+pdflatex --interaction nonstopmode %FILENAME% 
+
+pdflatex %FILENAME%
+
+if not exist %FILENAME%.pdf (
+  echo Not generate PDF, please look up to see if there were any errors
+  goto :END
+)
+
+::###########################################
+:: Clean
+::###########################################
+
+::DISABLED
+
+::echo.
+::echo Done compile, going to clean
+
+::echo.
+::call clean.cmd
+
+
+::###########################################
+:: Show it
+::###########################################
+
+echo Opening PDF
+%FILENAME%.pdf
+
+::###########################################
+:: END
+::###########################################
+:END
+
+echo.
+echo Done, going to exit
+pause
